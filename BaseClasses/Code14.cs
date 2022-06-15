ef96a296488a5312791f80e40f604860cd826892
ijungleboy <daniel.mettler@2sic.com>
2022-06-15 10:06:55 +0200

    linking enhancements and ServiceKit

--- BaseClasses/Code14.cs
+++ BaseClasses/Code14.cs
@@ -1,6 +1,6 @@
 using ToSic.Sxc.Services;
 
-public class Code14 : Custom.Code14<dynamic, Kit14> {
+public class Code14 : Custom.Code14<dynamic, ServiceKit14> {
   public string SayHello() {
     // Try to access the Kit
     var x = Kit.Convert.ToInt("27.4") + 2;
