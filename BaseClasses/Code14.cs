c61d3062a7a0b59b3be004947b7f020c1850d0e0
ijungleboy <daniel.mettler@2sic.com>
2022-06-16 18:04:16 +0200

    wip

--- BaseClasses/Code14.cs
+++ BaseClasses/Code14.cs
@@ -1,9 +1,10 @@
-using ToSic.Sxc.Services;
-
-public class Code14 : Custom.Hybrid.Code14<dynamic, ServiceKit14> {
+public class Code14 : Custom.Hybrid.Code14<dynamic, ToSic.Sxc.Services.ServiceKit14> {
   public string SayHello() {
     // Try to access the Kit
     var x = Kit.Convert.ToInt("27.4") + 2;
+
+    // this must fail
+    // var x = Convert.ToInt("27.4") + 2;
     return "Hello from shared functions! - got " + x;
   }
 }
