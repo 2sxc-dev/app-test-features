7f707472f6235fbc8359e83094db96f11845f6c7
ijungleboy <daniel.mettler@2sic.com>
2022-11-29 15:00:00 +0100

    work on wysiwyg 15 tests

---
+++ _Shared/TestHelpers.cs
@@ -0,0 +1,21 @@
+using ToSic.Sxc.Demo;
+using ToSic.Razor.Blade;
+using ToSic.Razor.Markup;
+
+// Important notes: 
+// - This class should have the same name as the file it's in
+public class TestHelpers : Custom.Hybrid.Code14 
+{
+  public ITag SubLinkWithIndicator(string paramName, string value, string title) {
+    var pParams = CmsContext.Page.Parameters;
+    var existingValue = pParams[paramName];
+    var isSet = existingValue == value;
+    return Tag.A()
+      .Href(Kit.Link.To(parameters: value == null ? pParams.Remove(paramName) : pParams.Set(paramName, value)))
+      .Wrap(
+        title,
+        " ",
+        isSet ? "✅" : ""
+      );
+  }
+}
