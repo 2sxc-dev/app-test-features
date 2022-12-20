02d9917a9541ca74092f68880df73a698678a3c0
ijungleboy <daniel.mettler@2sic.com>
2022-12-20 18:16:37 +0100

    fix test linking so it works in Oqtane for now

--- _Shared/TestLinker.cs
+++ _Shared/TestLinker.cs
@@ -9,13 +9,17 @@ public class TestLinker : Custom.Hybrid.Code14
   public ITag SubLinkWithIndicator(string paramName, string value, string title) {
     var pParams = CmsContext.Page.Parameters;
     var existingValue = pParams[paramName];
-    var isSet = existingValue == value;
+    var isSet = Text.Has(existingValue) && Text.Has(value)
+      && (existingValue == value || existingValue.Replace("%2f", "/") == value.Replace("%2f", "/"));
     return Tag.A()
       .Href(Kit.Link.To(parameters: value == null ? pParams.Remove(paramName) : pParams.Set(paramName, value)))
       .Wrap(
         title,
         " ",
         isSet ? "✅" : ""
+        // debug stuff:
+        // , "'" + existingValue + "'"
+        // , ",'" + value + "'"
       );
   }
 }
