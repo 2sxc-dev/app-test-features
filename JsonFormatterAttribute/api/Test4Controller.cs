e6d0ff3e1c547863732e31af2c6e71e0a88470e3
Tonći Vatavuk <tonci@sistemi.hr>
2023-02-23 07:32:32 +0100

    test JsonFormatterAttribute

---
+++ JsonFormatterAttribute/api/Test4Controller.cs
@@ -0,0 +1,45 @@
+#if NETCOREAPP // Oqtane
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+#else // DNN
+using System.Web.Http;
+using DotNetNuke.Web.Api;
+#endif
+using ToSic.Eav.Data;
+using ToSic.Sxc.WebApi;
+using ToSic.Sxc.Services; // Make it easier to use https://r.2sxc.org/services
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Dynlist = System.Collections.Generic.IEnumerable<dynamic>;
+
+
+[AllowAnonymous]
+[JsonFormatter(Casing = Casing.CamelCase)]
+public class Test4Controller : Custom.Hybrid.Api15
+{
+    [HttpGet]
+    public object GetEntity()
+    {
+        return (App.Data.List.First());
+    }
+
+    [HttpGet]
+    public object GetEntities()
+    {
+        return App.Data.List;
+    }
+
+    [HttpGet]
+    public object Test3()
+    {
+        var entity = App.Data.List.First();
+        return new EntityWrapper(entity, null);
+    }
+
+    [HttpGet]
+    public object Test4()
+    {
+        return AsList(App.Data).Select(d => new EntityWrapper(AsEntity(d), null));
+    }
+}
---
+++ JsonFormatterAttribute/api/Test8Controller.cs
@@ -0,0 +1,47 @@
+#if NETCOREAPP // Oqtane
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+#else // DNN
+using System.Web.Http;
+using DotNetNuke.Web.Api;
+#endif
+using ToSic.Eav.Data;
+using ToSic.Sxc.WebApi;
+using ToSic.Sxc.Services; // Make it easier to use https://r.2sxc.org/services
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Dynlist = System.Collections.Generic.IEnumerable<dynamic>;
+
+
+[AllowAnonymous]
+[JsonFormatter(AutoConvertEntity = false, Casing = Casing.PascalCase)]
+public class Test8Controller : Custom.Hybrid.Api15
+{
+    [HttpGet]
+    [JsonFormatter(AutoConvertEntity = true, Casing = Casing.PascalCase)]
+    public object GetEntity()
+    {
+        return (App.Data.List.First());
+    }
+
+    [HttpGet]
+    [JsonFormatter(AutoConvertEntity = false, Casing = Casing.CamelCase)]
+    public object GetEntities()
+    {
+        return App.Data.List;
+    }
+
+    [HttpGet]
+    public object Test3()
+    {
+        var entity = App.Data.List.First();
+        return new EntityWrapper(entity, null);
+    }
+
+    [HttpGet]
+    public object Test4()
+    {
+        return AsList(App.Data).Select(d => new EntityWrapper(AsEntity(d), null));
+    }
+}
