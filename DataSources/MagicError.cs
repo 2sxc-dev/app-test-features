2d1910bbc99b2da9403b6d1db82cc3aaa5917aeb
ijungleboy <daniel.mettler@2sic.com>
2023-03-23 18:23:20 +0100

    testing errors...

---
+++ DataSources/MagicError.cs
@@ -0,0 +1,33 @@
+using ToSic.Eav.DataSources;
+using ToSic.Eav.Data.Raw;
+using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav.DataSources;
+using ToSic.Eav.DataSources.Queries;
+
+namespace App {
+  // Test for scenarios with an Error DataSource
+  public class MagicError : Custom.DataSources.DataSource15 {
+
+    public MagicError(MyServices services) : base(services, "My.Magic")
+    {
+      ProvideOut(() => {
+        var newItem = new RawEntity() {
+          Id = 27,
+          // x = x
+          Values = new Dictionary<string, object> {
+            {"Title", "Hello from MagicDataSource"},
+            {"FavoriteNumber", FavoriteNumber},
+          }
+        };
+        return Enumerable.Repeat(newItem, AmountOfItems).ToList();
+      });
+    }
+
+    [Configuration(Fallback = 3)]
+    public int AmountOfItems { get { return Configuration.GetThis(3); } }
+
+    [Configuration(Fallback = 42)]
+    public int FavoriteNumber { get { return Configuration.GetThis(42); } }
+  }
+}
