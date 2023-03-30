f31f5fada5229c2985b537e75eb93b19cf96204b
ijungleboy <daniel.mettler@2sic.com>
2023-03-30 18:21:12 +0200

    tweak api - filter

--- Services/Toolbar-Service/ToolbarServiceTests.cs
+++ Services/Toolbar-Service/ToolbarServiceTests.cs
@@ -18,6 +18,21 @@ public class ToolbarServiceTests: Custom.Hybrid.Code14
     );
   }
 
+  // Show a test according to standard output
+  public dynamic TbTestAsVisibleTag(string message, string variation, dynamic toolbar) {
+    return Tag.Li(
+      message + " (",
+      Tag.Strong(variation),
+      ")",
+      toolbar.AsTag(),
+      Tag.Br(),
+      "JSON: ",
+      toolbar.AsJson(),
+      Tag.Hr()
+    );
+  }
+
+
   public dynamic TbTestOnLi(string message, dynamic toolbar) {
     return Tag.Li(
       message + " ",
