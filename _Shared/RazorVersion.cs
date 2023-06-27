a9cb685f0e75ffadf6a28210f579104fca9463d8
ijungleboy <daniel.mettler@2sic.com>
2023-06-27 16:10:43 +0200

    Option to make base class a different Razor

---
+++ _Shared/RazorVersion.cs
@@ -0,0 +1,18 @@
+using ToSic.Sxc.Demo;
+using ToSic.Razor.Blade;
+
+public class RazorVersion : Custom.Hybrid.Code14 
+{
+  public IHtmlTag RazorWithVersionPicker(object page) {
+    var tags = Kit.HtmlTags;
+    var par = CmsContext.Page.Parameters.Remove("Razor12").Remove("Razor14").Remove("Razor16");
+    return tags.RawHtml(
+      "(Razor Version: ",
+      page is Custom.Hybrid.Razor12 ? tags.U("12") as IHtmlTag: tags.A("12").Href(Link.To(parameters: par.Set("Razor12", "true"))),
+      " ",
+      page is Custom.Hybrid.Razor14 ? tags.U("14") as IHtmlTag: tags.A("14").Href(Link.To(parameters: par.Set("Razor14", "true"))),
+      " ",
+      page is Custom.Hybrid.Razor16 ? tags.U("16") as IHtmlTag: tags.A("16").Href(Link.To(parameters: par.Set("Razor16", "true"))),
+     ")");
+  }
+}
