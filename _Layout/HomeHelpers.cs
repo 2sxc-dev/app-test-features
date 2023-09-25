0e6a9274224455e7bfeb688a808813e3648cbee7
iJungleboy <daniel.mettler@2sic.com>
2023-09-25 07:26:07 +0200

    improve handling Default/_Default

---
+++ _Layout/HomeHelpers.cs
@@ -0,0 +1,29 @@
+using ToSic.Sxc.Demo;
+using ToSic.Razor.Blade;
+
+public class HomeHelpers : Custom.Hybrid.CodeTyped
+{
+  const string Home = "_Home.cshtml";
+  const string OldDefault = "_default.cshtml";
+  const string NewDefault = "Default.cshtml";
+  public void GetAllPaths(out string root, out string cshtml, out string fullPath, out string testPath) {
+    testPath = MyPage.Parameters["TestPath"];
+
+    // If no path was given, use the current path
+    root = App.Folder.PhysicalPath + (Text.Has(testPath) ? "/" + testPath : "/");
+    fullPath = root + Home;
+
+    // Show Home if no path was given
+    cshtml = Text.First(testPath, Home);
+    
+    if (!cshtml.Contains(".cshtml")) {
+      cshtml = cshtml + OldDefault;
+      fullPath = root + OldDefault; // already has the path, just missing the file
+      // Check if _default exists...
+      if (!System.IO.File.Exists(fullPath)) {
+        cshtml = cshtml.Replace(OldDefault, NewDefault);
+        root = root.Replace(OldDefault, NewDefault);
+      }
+    }
+  }
+}
