d38c780bf6df55821a0a0796ca83b02350e99bea
iJungleboy <daniel.mettler@2sic.com>
2023-09-28 18:46:56 +0200

    dummy samples for new feature #AppCode

---
+++ AppCode/DemoService.cs
@@ -0,0 +1,5 @@
+public class DemoService : Custom.Hybrid.Code14 {
+  public string SayHello() {
+    return new LinkHelper().SayHello();
+  }
+}
---
+++ DynamicCode/WithAppCode/WithAppCode.cs
@@ -0,0 +1,5 @@
+public class WithAppCode : Custom.Hybrid.CodeTyped {
+  public string SayHello() {
+    return new LinkHelper().SayHello();
+  }
+}
