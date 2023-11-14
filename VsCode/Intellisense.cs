f27d3f1d4859c6fe799ecc6c5553fb522766bc55
iJungleboy <daniel.mettler@2sic.com>
2023-11-14 18:28:11 +0100

    minor wip - intellisense experimens

---
+++ VsCode/Intellisense.cs
@@ -0,0 +1,13 @@
+using ToSic.Sxc;
+
+public class TestIntellisense
+{
+  // Class members go here
+  public void TestBadApis()
+  {
+    // this namespaces is [PrivateApi] and WIP
+    ToSic.Sxc.Adam. x = null;
+
+    var y = ToSic.Sxc.Configuration.Features.BuiltInFeatures.CdnSourcePublic;
+  }
+}
