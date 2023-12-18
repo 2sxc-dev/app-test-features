06d468623022baa450de980e1ac64b94aa8c9a27
iJungleboy <daniel.mettler@2sic.com>
2023-12-18 16:44:25 +0100

    wip tests with code

---
+++ AppCode/ShowTestData.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApp.Code
+{
+  public class ShowTestData : Custom.Hybrid.CodeTyped
+  {
+    /// <summary>
+    /// Show a boolean value as an emoji
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// 
+    public string BoolMoji(bool value) {
+      return value ? "✅" : "❌";
+    }
+
+  }
+}
