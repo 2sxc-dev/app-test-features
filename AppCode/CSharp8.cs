5d595bebf87f0c48f4521a76716684c987f05c2f
iJungleboy <daniel.mettler@2sic.com>
2023-12-20 18:13:16 +0100

    Add c# 7.3 and C# 8 tests

---
+++ AppCode/CSharp8.cs
@@ -0,0 +1,20 @@
+namespace MyApp.Code
+{
+
+
+  public class CSharp8 {
+    public string Name => "Hello";
+
+    public string Name2 {
+      get { return  "Hello"; }
+    }
+
+    public (int x, int y) GetPoint() => (1, 2);
+
+    public string NullCoalescingAssigment(string? value) {
+      value ??= "Hello";
+      return value;
+    }
+  }
+
+}
