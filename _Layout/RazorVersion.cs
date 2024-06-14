c0122ced6d254512fd723afabdb8b0bc61027eaf
iJungleboy <daniel.mettler@2sic.com>
2024-06-14 07:43:05 +0200

    minor

--- _Layout/RazorVersion.cs
+++ _Layout/RazorVersion.cs
@@ -1,4 +1,3 @@
-using ToSic.Sxc.Demo;
 using ToSic.Razor.Blade;
 
 public class RazorVersion : Custom.Hybrid.Code14 
@@ -6,13 +5,21 @@ public class RazorVersion : Custom.Hybrid.Code14
   public IHtmlTag RazorWithVersionPicker(object page) {
     var tags = Kit.HtmlTags;
     var par = CmsContext.Page.Parameters.Remove("Razor12").Remove("Razor14").Remove("Razor16");
-    return tags.RawHtml(
+    return tags.Span(
       "(Razor Version: ",
-      page is Custom.Hybrid.Razor12 ? tags.U("12") as IHtmlTag: tags.A("12").Href(Link.To(parameters: par.Set("Razor12", "true"))),
+      page is Custom.Hybrid.Razor12
+        ? tags.U("12") as IHtmlTag
+        : tags.A("12").Href(Link.To(parameters: par.Set("Razor12", "true"))),
       " ",
-      page is Custom.Hybrid.Razor14 ? tags.U("14") as IHtmlTag: tags.A("14").Href(Link.To(parameters: par.Set("Razor14", "true"))),
+      page is Custom.Hybrid.Razor14
+        ? tags.U("14") as IHtmlTag
+        : tags.A("14").Href(Link.To(parameters: par.Set("Razor14", "true"))),
       " ",
-      page is Custom.Hybrid.RazorTyped ? tags.U("Typed") as IHtmlTag: tags.A("Typed").Href(Link.To(parameters: par.Set("Razor16", "true"))),
-     ")");
+      page is Custom.Hybrid.RazorTyped
+        ? tags.U("Typed") as IHtmlTag
+        : tags.A("Typed").Href(Link.To(parameters: par.Set("Razor16", "true"))),
+     ")")
+     // .Style("width: 100%;")
+     .Attr(Kit.Toolbar.Default().Settings(hover: "left"));
   }
 }
