e0212ccfa9e525dac982c1aaaac073f77bde2e99
Tonći Vatavuk <tonci@sistemi.hr>
2024-11-14 18:04:44 +0100

    SecureEndpoint wip

--- WebApi-SecureEndpointAttribute/api/SecureEndpointController.cs
+++ WebApi-SecureEndpointAttribute/api/SecureEndpointController.cs
@@ -16,10 +16,25 @@ public class SecureEndpointController : Custom.Hybrid.ApiTyped
     public string Field2 { get; set; }
   }
 
+  public class Form2Data
+  {
+    public string P1 { get; set; }
+    public string P2 { get; set; }
+    public string P3 { get; set; }
+    public int P4 { get; set; }
+  }
+
   [HttpPost]
   [SecureEndpoint]
   public object SubmitForm(Form1Data data)
   {
     return data;
+  }
+
+  [HttpPost]
+  [SecureEndpoint]
+  public object SubmitForm2(int q1, Form2Data data, string q2, string q3 = "default")
+  {
+    return new { q1, data, q2, q3 };
   }
 }
