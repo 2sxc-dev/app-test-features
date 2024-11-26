0c809ee224d9a07a82d6bf2fdb5a550c8007df01
Tonći Vatavuk <tonci@sistemi.hr>
2024-11-26 12:01:05 +0100

    SecureEndpoint more tests

---
+++ WebApi-SecureEndpointAttribute/api/SecureEndpoint2Controller.cs
@@ -0,0 +1,40 @@
+#if NETCOREAPP // Oqtane
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToSic.Sxc.WebApi.ActionFilters;
+#else // DNN
+using System.Web.Http;
+using ToSic.Sxc.Dnn.WebApi.Internal.SecureEndpoint;
+#endif
+using System.Linq;
+
+[AllowAnonymous]
+[SecureEndpoint]
+public class SecureEndpoint2Controller : Custom.Hybrid.ApiTyped
+{
+  public class Form21Data
+  {
+    public string Field1 { get; set; }
+    public string Field2 { get; set; }
+  }
+
+  public class Form22Data
+  {
+    public string P1 { get; set; }
+    public string P2 { get; set; }
+    public string P3 { get; set; }
+    public int P4 { get; set; }
+  }
+
+  [HttpPost]
+  public object SubmitForm(Form21Data data)
+  {
+    return data;
+  }
+
+  [HttpPost]
+  public object SubmitForm2(int q1, Form22Data data, string q2, string q3 = "default")
+  {
+    return new { q1, data, q2, q3 };
+  }
+}
