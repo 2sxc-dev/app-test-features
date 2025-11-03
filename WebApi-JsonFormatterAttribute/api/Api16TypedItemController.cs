62b88b06e444cb50ce5fef7f7e096d723427d6f2
iJungleboy <daniel.mettler@2sic.com>
2025-11-03 22:56:40 +0100

    improve tests w/multi-language state...

--- WebApi-JsonFormatterAttribute/api/Api16TypedItemController.cs
+++ WebApi-JsonFormatterAttribute/api/Api16TypedItemController.cs
@@ -5,13 +5,23 @@ using Microsoft.AspNetCore.Mvc;
 using System.Web.Http;
 #endif
 using System.Linq;
+using ToSic.Sxc.Data;
+using System.Collections.Generic;
 
 
 [AllowAnonymous]
 public class Api16TypedItemController : Custom.Hybrid.ApiTyped
 {
   [HttpGet]
-  public object GetEntity() => AsItem(App.Data.List.First());
+  public object GetEntity()
+  {
+    var item = AsItem(App.Data.List.First());
+    Log.Add($"returning item id {item.Id} - Description: {item.String("Description")}");
+
+    // var list = new List<ITypedItem> { item };
+    // var json = Kit.Json.ToJson(item as object);
+    return item;
+  }
 
   [HttpGet]
   public object GetEntities() => AsItems(App.Data.List.Take(5));
