01378ef4e572df1b7c9cdd25c762e4b7d7f4e7e8
iJungleboy <daniel.mettler@2sic.com>
2025-12-01 23:46:38 +0100

    improve REST tess

--- AppCode/RestTests/RestTest.cs
+++ AppCode/RestTests/RestTest.cs
@@ -2,12 +2,16 @@ namespace AppCode.RestTests
 {
   public class RestTest
   {
+    public string Category { get; set; } = "Basic";
+
     public string Title { get; set; }
 
     public string Description { get; set; }
 
+    public string ContentType { get; set; } = "RestTestBook";
+
     public string Select { get; set; }
 
-    public string ContentType { get; set; } = "RestTestBook";
+    public string Filter { get; set;  }
   }
 }
