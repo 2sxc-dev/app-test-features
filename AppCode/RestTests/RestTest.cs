12202a7195ba2776b04ac6e44d6d47034cf4be4c
iJungleboy <daniel.mettler@2sic.com>
2025-12-01 21:41:58 +0100

    first REST ODATA tests

---
+++ AppCode/RestTests/RestTest.cs
@@ -0,0 +1,13 @@
+namespace AppCode.RestTests
+{
+  public class RestTest
+  {
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Select { get; set; }
+
+    public string ContentType { get; set; } = "RestTestBook";
+  }
+}
