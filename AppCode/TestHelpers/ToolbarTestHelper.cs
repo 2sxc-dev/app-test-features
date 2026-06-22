ed3f2119de4e7687eff62b590b2a148349bc65de
iJungleboy <daniel.mettler@2sic.com>
2026-06-22 13:18:22 +0200

    improve toolbar icon tests w/comments

--- AppCode/TestHelpers/ToolbarTestHelper.cs
+++ AppCode/TestHelpers/ToolbarTestHelper.cs
@@ -6,22 +6,42 @@ namespace AppCode.TestHelpers
 {
   public class ToolbarTestHelper: Custom.Hybrid.CodeTyped
   {
-    // Show a test according to standard output
-    public ITag TbTestAsTag(string message, IToolbarBuilder toolbar) {
-      return Tag.Li(
+    /// <summary>
+    /// Show a test according to standard output
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="toolbar">The toolbar builder</param>
+    /// <param name="showJson">Whether to show the JSON representation</param>
+    /// <returns>The generated tag</returns>
+    public ITag ShowLiAndToolbar(string message, IToolbarBuilder toolbar) =>
+      TbTestAsTag(message, toolbar, false);
+      
+    /// <summary>
+    /// Show a test according to standard output
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="toolbar">The toolbar builder</param>
+    /// <param name="showJson">Whether to show the JSON representation</param>
+    /// <returns>The generated tag</returns>
+    public ITag TbTestAsTag(string message, IToolbarBuilder toolbar, bool showJson = true) =>
+      Tag.Li(
         message + " ",
         toolbar.AsTag(),
         Tag.Br(),
         "JSON: ",
         Tag.Code(
-          toolbar.AsJson()
+          showJson ? toolbar.AsJson().ToString() : "(skipped for brevity)"
         ).Attr(toolbar),
-        // toolbar.AsJson(),
         Tag.Hr()
       );
-    }
 
-    // Show a test according to standard output
+    /// <summary>
+    /// Show a test according to standard output with a visible variation
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="variation">The variation to display</param>
+    /// <param name="toolbar">The toolbar builder</param>
+    /// <returns>The generated tag</returns>
     public ITag TbTestAsVisibleTag(string message, string variation, IToolbarBuilder toolbar) {
       return Tag.Li(
         message + " (",
@@ -33,13 +53,16 @@ namespace AppCode.TestHelpers
         Tag.Pre(
           toolbar.AsJson()
         ),
-        // "JSON: ",
-        // toolbar.AsJson(),
         Tag.Hr()
       );
     }
 
-
+    /// <summary>
+    /// Show a test on a list item
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="toolbar">The toolbar builder</param>
+    /// <returns>The generated tag</returns>
     public ITag TbTestOnLi(string message, IToolbarBuilder toolbar) {
       return Tag.Li(
         message + " ",
